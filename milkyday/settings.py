b36beef17a18458b708643351fb1d6fefabb693f
Ajay Sarathy <saraties2088@gmail.com>
2020-11-20 02:24:47 -1000

    arrow bug is hopefully fixed

--- milkyday/settings.py
+++ milkyday/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '4jtj#tdndxgx5czt()s_=u_a_ss3ozz30vte0*p=b&!8@tt3vz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['milkyday.herokuapp.com', '127.0.0.1' ]
 
