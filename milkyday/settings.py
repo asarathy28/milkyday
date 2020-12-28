7f4bb85ad47019d8e53c3cb80a9ba456da06066a
Ajay Sarathy <saraties2088@gmail.com>
2020-12-28 13:20:40 -0500

    add sentry

--- milkyday/settings.py
+++ milkyday/settings.py
@@ -12,7 +12,18 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import dotenv
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
+sentry_sdk.init(
+    dsn="https://63e024a471124ddab7e469809073994f@o455512.ingest.sentry.io/5447168",
+    integrations=[DjangoIntegration()],
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    #send_default_pii=True
+)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
